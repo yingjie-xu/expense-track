ed8a1db72884c26eb17dbfad8b90ecce19d44f53
yingjie-xu <xuyingjie619@gmail.com>
2020-07-21 01:18:05 -0400

    Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
-import { Button } from 'antd';
 import './App.less';
+import AppRouter from './router/AppRouter';
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <AppRouter />
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import storeConfig from './redux/StoreConfig';  
 import * as serviceWorker from './serviceWorker';
+import { addExpense } from './redux/ExpenseActions';
+
+const store = storeConfig();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+
+store.dispatch(addExpense({description: "Gas Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+store.dispatch(addExpense({description: "Water Bill", note: "July", amount: 3000}))
+console.log(store.getState())
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
